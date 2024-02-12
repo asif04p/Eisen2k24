fb6f32b31f97e0f24d13f2eb13fc43fac65eb1d0
Asif Pathan <asif6302104@gmail.com>
2024-02-12 17:29:36 +0530

    removed preloader

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-// App.js
-import React, { useEffect, useState } from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
@@ -7,25 +5,12 @@ import Home from './Pages/Home';
 import About from './Pages/About';
 import Events from './Pages/Events';
 import Contact from './Pages/Contact';
-import Preloader from './Components/Preloaders/Preloader';  // Import the Preloader component
 import { Routes, Route, BrowserRouter as Router, Navigate } from 'react-router-dom';
 
 function App() {
-  const [contentLoaded, setContentLoaded] = useState(false);
-
-  // Simulating content loading with a delay (you can replace this with actual loading logic)
-  useEffect(() => {
-    const loadingTimeout = setTimeout(() => {
-      setContentLoaded(true);
-    }, 5000); // Adjust the timeout as needed
-
-    return () => clearTimeout(loadingTimeout);
-  }, []);
-
   return (
-    <div className={`App ${contentLoaded ? 'content-loaded' : ''}`}>
+    <div className="App">
       <Router>
-        <Preloader />
         <Navbar />
         <Routes>
           <Route path="/" element={<Navigate to="/home" />} />
