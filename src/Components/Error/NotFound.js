bbc599b3f2ec5bd648a2df7089d1703283cbc5cd
Asif Pathan <asif6302104@gmail.com>
2024-02-13 20:49:55 +0530

    changes in Notfound

--- src/Components/Error/NotFound.js
+++ src/Components/Error/NotFound.js
@@ -8,15 +8,16 @@ const NotFound = () => {
       <img
         src={process.env.PUBLIC_URL + '/oops.png'}
         alt="404 Not Found"
+        className='oops-image'
         style={{ maxWidth: '60%', height: 'auto' }}
       />
       <h2 className='oops-heading'>Oops! You've come to the wrong place.</h2>
       <p className='oops-text'>
         If you're looking for Events you can check out our{' '}
-        <Link to="/events">events page</Link> <br/>
+        <Link to="/events">Events</Link> <br/>
         <br/>
         Or navigate back to the{' '}
-        <Link to="/">home page</Link>.
+        <Link to="/">Home</Link>
       </p>
     </div>
   );
